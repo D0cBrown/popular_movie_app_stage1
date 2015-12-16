57b57d65d90690ab005937bfd1d946cc172e785e
francesco garzoli <francesco.garzoli@gmail.com>
2015-12-16 11:28:19 +0100

    initial commit

---
+++ app/src/main/java/popularmovie/project/garzoli/it/popularmovie/BundleKeys.java
@@ -0,0 +1,11 @@
+package popularmovie.project.garzoli.it.popularmovie;
+
+/**
+ * @author Francesco Garzoli
+ * Date:   09-12-2015
+ */
+public abstract class BundleKeys {
+
+    public static final String MOVIE = "movie";
+
+}
